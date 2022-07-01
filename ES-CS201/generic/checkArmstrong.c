ab879df1e8940b5656f1435ce248af5048c3aaa3
Aryan Singh <zer0as@protonmail.com>
2022-07-01 12:57:04 +0530

    new course starts this month!

--- ES-CS201/generic/checkArmstrong.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-
-void main() {
-    int i_num1, i_num2, i_num3, i_rem; 
-}
--- ES-CS201/lab-assignment/1/printName.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-/* program to print your name */
-
-int main() {
-    printf("Aryan Singh\n");
-    return 0;
-}
