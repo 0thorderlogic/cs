dd5ef7f5b750ccf19b7e9307592298033a16046c
0thorderlogic <aoneone5@protonmail.com>
2023-03-30 14:44:41 +0530

    loosers need comments

--- computer_architecture/half_adder/halfadder_tb.v
+++ computer_architecture/half_adder/halfadder_tb.v
@@ -1,15 +1,12 @@
 `timescale 1ns / 1ps
 
 module halfadder_tb;
-	// Inputs
 	reg a;
 	reg b;
 
-	// Outputs
 	wire s;
 	wire c;
 
-	// Instantiate the Unit Under Test (UUT)
 	halfadder uut (
 		.a(a), 
 		.b(b), 
@@ -18,38 +15,26 @@ module halfadder_tb;
 	);
 
 	initial begin
-		// Initialize Inputs
 		a = 0;
 		b = 0;
 
-		// Wait 100 ns for global reset to finish
 		#100;
         
-		// Add stimulus here
-		// Initialize Inputs
 		a = 0;
 		b = 1;
 
-		// Wait 100 ns for global reset to finish
 		#100;
         
-		// Add stimulus here
-		// Initialize Inputs
 		a = 1;
 		b = 0;
 
-		// Wait 100 ns for global reset to finish
 		#100;
         
-		// Add stimulus here
-		// Initialize Inputs
 		a = 1;
 		b = 1;
 
-		// Wait 100 ns for global reset to finish
 		#100;
         
-		// Add stimulus here
 
 	end
 
